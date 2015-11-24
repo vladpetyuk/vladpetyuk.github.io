83c7dcc27a70c29918661878ac2f518c12d9ca66
Vlad Petyuk <petyuk@gmail.com>
2015-11-23 22:45:33 -0800

    making path settings flexible

--- _knitposts.R
+++ _knitposts.R
@@ -9,17 +9,6 @@
 
 # run ./knitpages.R to update all knitr files that need to be updated.
 
-KnitPost <- function(input, outfile, base.url="/") {
-  # this function is a modified version of an example here:
-  # http://jfisher-usgs.github.com/r/2012/07/03/knitr-jekyll/
-  require(knitr);
-  opts_knit$set(base.url = base.url)
-  fig.path <- paste0("blog/figs/", sub(".Rmd$", "", basename(input)), "/")
-  opts_chunk$set(fig.path = fig.path)
-  opts_chunk$set(fig.cap = "figure")
-  render_jekyll()
-  knit(input, outfile, envir = parent.frame())
-}
 
 # path to Rmd files
 # blog_path <- "blog/" # this works if you are in the "vladpetyuk.github.io"
@@ -27,6 +16,23 @@ KnitPost <- function(input, outfile, base.url="/") {
 # I'll use absolute for the same convenience of running from any directory.
 # Perhaps I'll update this later to make sure it runs on multiple different comps.
 blog_path <- "/Users/d3m629/Google Drive/vladpetyuk.github.io/blog/" # absolute!!
+
+
+
+KnitPost <- function(input, outfile, base.url="/") {
+  # this function is a modified version of an example here:
+  # http://jfisher-usgs.github.com/r/2012/07/03/knitr-jekyll/
+  require(knitr)
+  opts_knit$set(base.dir = "/Users/d3m629/Google Drive/vladpetyuk.github.io")
+  opts_knit$set(base.url = base.url)
+  fig.path <- paste0("blog/figs/", sub(".Rmd$", "", basename(input)), "/")
+  # fig.path <- paste0(blog_path, "figs/", sub(".Rmd$", "", basename(input)), "/")
+  opts_chunk$set(fig.path = fig.path)
+  opts_chunk$set(fig.cap = "figure")
+  render_jekyll()
+  knit(input, outfile, envir = parent.frame())
+}
+
 
 for (infile in list.files(paste0(blog_path,"_R/"), pattern="*.Rmd", full.names=TRUE)) {
   outfile = paste0(blog_path,"_posts/", sub(".Rmd$", ".md", basename(infile)))
